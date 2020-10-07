9f6fb41ce2ae1a3a2a92751955198395022ca12b
joshuayannix <joshuayannix@gmail.com>
2020-10-07 09:47:47 -0400

    banner

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
 import Row from './Row';
+import Banner from './Banner';
 import requests from './requests';
 
 function App() {
   return (
     <div className="App">
+
       <h1>hello world</h1>
+      <Banner />
       <Row 
         title='Trending'
         fetchUrl={requests.fetchTrending}
+        isLargeRow
       />
       <Row 
         title='Netflix Originals'
@@ -17,10 +21,29 @@ function App() {
       />
       <Row 
         title='Top Rated'
-        fetchUrl={requests.fetchTopRated}
+        fetchUrl={requests.fetchTopRated}        
       />
-      
-      
+      <Row 
+        title='Action Movies'
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row 
+        title='Comedy Movies'
+        fetchUrl={requests.fetchComedyMovies}
+      />
+      <Row 
+        title='Horror Movies'
+        fetchUrl={requests.fetchHorrorMovies}
+      />
+      <Row 
+        title='Romance Movies'
+        fetchUrl={requests.fetchRomanceMovies}
+      />
+      <Row 
+        title='Documentaries'
+        fetchUrl={requests.fetchDocumentaries}
+      />
+
     </div>
   );
 }
