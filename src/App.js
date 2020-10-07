5ed788a17de599bad0a2ee22eed479bf8dbbbefe
joshuayannix <joshuayannix@gmail.com>
2020-10-07 07:38:59 -0400

    rows showing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
+import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
+      <Row 
+        title='Trending'
+        fetchUrl={requests.fetchTrending}
+      />
+      <Row 
+        title='Netflix Originals'
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row 
+        title='Top Rated'
+        fetchUrl={requests.fetchTopRated}
+      />
+      
+      
     </div>
   );
 }
---
+++ src/requests.js
@@ -0,0 +1,15 @@
+const API_KEY = 'c94960b3bb8a8100ce74627573c727d1';
+// https://api.themoviedb.org/3/movie/550?api_key=c94960b3bb8a8100ce74627573c727d1
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchROmanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
