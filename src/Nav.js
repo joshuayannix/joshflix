7caf24ccd3fa83f5b5e2493bb146776b56358af7
joshuayannix <joshuayannix@gmail.com>
2020-12-12 20:43:14 -0500

    fix anon function issue with scroll and link

--- src/Nav.js
+++ src/Nav.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from 'react';
 import './Nav.css';
 import joshflix from './joshflix.png';
 import { auth } from './firebase';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { selectUser } from './features/userSlice';
 import { Link } from 'react-router-dom';
+import { Avatar } from '@material-ui/core';
+
 
 function Nav() {
   const user = useSelector(selectUser);
@@ -17,29 +19,53 @@ function Nav() {
 
   const [show, handleShow] = useState(false);
 
+  const scrollHandler = e => {
+    if(window.scrollY > 100) {
+      handleShow(true);
+    } else handleShow(false);
+  }
+
   useEffect(() => {
-    window.addEventListener('scroll', () => {
-      if(window.scrollY > 100) {
-        handleShow(true);
-      } else handleShow(false);
-    });
+    window.addEventListener('scroll', scrollHandler);
     return () => {
-      window.removeEventListener('scroll');
+      window.removeEventListener('scroll', scrollHandler);
     };
   }, []);
 
   return (
-    <div className={`nav ${show && 'nav__black'}`}>     
-
+    <div 
+      className={`nav ${show && 'nav__black'}`}
+    >     
       <Link to='/'>      
         <img  
           className='nav__logo' 
           src={joshflix} 
-          alt="amazon logo"
+          alt="netflix logo"
         />
       </Link> 
 
-      
+      <div className="header__nav">
+        {user ? (
+          <>
+            <Avatar           
+          src={user.photo} 
+          className='nav__avatar'
+        /> 
+          </>
+        ) : (
+          <div></div>
+        )}
+        
+        <Link to={!user && '/login'} className='nav__link'>
+          <div 
+            onClick={handleAuthentication}
+            className="header__option"
+          >
+            <span className='nav__username'>Hello, {user ? user.displayName : 'Guest. Sign In'}</span>
+            
+          </div>
+        </Link>                   
+      </div>   
       
     </div>
   )
